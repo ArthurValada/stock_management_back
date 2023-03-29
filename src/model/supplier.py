9ca31205e8b2b5f73284cf26f5733805acb52aea
ArthurValada <arthur.c.valadares@gmail.com!>
2023-03-29 16:33:24 -0300

    Correção de notação de tipos com colchetes no caso de listas.

--- src/model/supplier.py
+++ src/model/supplier.py
@@ -1,11 +1,10 @@
 class Supplier:
 
-    def __init__(self, supplier_id: int, name: str, fk_employee_id: int, contact_ids: list(int),active: bool,
-                 fks_batchs_ids: list(int) = None):
+    def __init__(self, supplier_id: int, name: str, fk_employee_id: int, contact_ids: list[int], active: bool,
+                 fks_batchs_ids: list[int] = None):
         self.id: int = supplier_id
         self.name: str = name
         self.fk_registered_by_employee_id: int = fk_employee_id
-        self.contacts: list(int) = contact_ids
-        self.provider_batchs_ids: list(int) = fks_batchs_ids
+        self.contacts: list[int] = contact_ids
+        self.provider_batchs_ids: list[int] = fks_batchs_ids
         self.active: bool = active
-
