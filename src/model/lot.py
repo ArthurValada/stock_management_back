fcd8730819b15aaf9104dae941402af3deb4cbe4
ArthurValada <arthur.c.valadares@gmail.com!>
2023-03-29 16:02:55 -0300

    Corrção de valores padrão.

--- src/model/lot.py
+++ src/model/lot.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 class Lot:
 
-    def __init__(self, lot_id: int, purchase_price: float, quantity: int, active: bool,
-                 expiration_date: datetime = None, fk_supplier_id: int = None):
+    def __init__(self, lot_id: int, purchase_price: float, quantity: int, active: bool, fk_supplier_id: int,
+                 expiration_date: datetime = None):
         self.id: int = lot_id
         self.quantity: int = quantity
         self.purchase_price: float = purchase_price
