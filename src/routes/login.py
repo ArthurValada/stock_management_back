73b3873bffb767a587af16a15894b520a32ea73a
Ícaro Bruno <ibsc013@gmail.com>
2023-04-02 02:43:15 -0300

    Login corrigido para o BD

--- src/routes/login.py
+++ src/routes/login.py
@@ -1,14 +1,14 @@
 from flask import abort, make_response
+from src.model import employee as emp
 
 
 def login_name(name, password):
-    # same_name = filter(lambda x: True if x.name == name else False, STORAGE.values())
-    # if not same_name:
-    #     abort(404, f'No employee named {name} found')
-    #
-    # for employee in same_name:
-    #     if employee.password == password:
-    #         return make_response(f'{name} succefully logged on!', 200)
-    #
-    # abort(401, "The given password doesn't match the given name")
-    pass
+    same_name = emp.Employee.query.filter_by(name=name)
+    if not same_name:
+        abort(404, f'No employee named {name} found')
+
+    for employee in same_name:
+        if employee.password == password:
+            return make_response(f'{name} succefully logged on!', 200)
+
+    abort(401, "The given password doesn't match the given name")
