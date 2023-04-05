ee2ee4d23da06f7e4d339fa1aeee65f1ddc3e1da
ArthurValada <arthur.c.valadares@gmail.com!>
2023-04-05 14:43:56 -0300

    Mudança no método login de employee para funcionar com id.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from src import connexion_app, db
+from datetime import datetime
 
 if __name__ == '__main__':
     with connexion_app.app.app_context():
+        from src import employee
+
+        db.session.add(employee.Employee(id=1, name="Sla", is_active=True, date_of_birth=datetime.utcnow().date(),
+                                         sex="M", cpf="00000000000", password="password"))
         print(db.metadata.tables.keys())
 
     connexion_app.run(port=8001, debug=True)
--- src/routes/login.py
+++ src/routes/login.py
@@ -2,13 +2,13 @@ from flask import abort, make_response
 from src.model import employee as emp
 
 
-def login_name(name, password):
-    same_name = emp.Employee.query.filter_by(name=name).all()
-    if not same_name:
-        abort(404, f'No employee named {name} found')
+def login_id(id, password):
+    same_id = emp.Employee.query.filter_by(id=id).all()
+    if not same_id:
+        abort(404, f'No employee with id {id} found')
 
-    for employee in same_name:
+    for employee in same_id:
         if employee.password == password:
-            return make_response(f'{name} succefully logged on!', 200)
+            return make_response(f'{id} succefully logged on!', 200)
 
     abort(401, "The given password doesn't match the given name")
